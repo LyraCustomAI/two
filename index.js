8849d025d1708062d5b5aa74197aa43fc5e6727a
LyraCustomAI <essence.littlelady@gmail.com>
2025-12-02 14:33:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,9 @@
-import express from "express";
-import cors from "cors";
-import config from ".src/config.js";
+import dotenv from "dotenv";
+dotenv.config();
 
-import photos from ".src/routes/photos.js";
-import gifts from ".src/routes/gifts.js";
-import messages from ".src/routes/messages.js";
-import visitors from ".src/routes/visitors.js";
-import friends from ".src/routes/friends.js";
-import forum from ".src/routes/forum.js";
-import votes from ".src/routes/votes.js";
-import auth from ".src/routes/auth.js";
+import app from "./src/app.js";
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-// Routes
-app.use("/photos", photos);
-app.use("/gifts", gifts);
-app.use("/messages", messages);
-app.use("/visitors", visitors);
-app.use("/friends", friends);
-app.use("/forum", forum);
-app.use("/votes", votes);
-app.use("/auth", auth);
-
-app.listen(config.port, () => {
-  console.log(`🔥 TWO.lt API online ant ${config.port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🔥 TWO.lt API online ant ${PORT}`);
 });
