8f4499b65c59a8d7566c35a1d54613ee402c9f09
LyraCustomAI <essence.littlelady@gmail.com>
2025-12-02 14:28:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import express from "express";
 import cors from "cors";
-import config from "./config.js";
+import config from ".src/config.js";
 
-import photos from "./routes/photos.js";
-import gifts from "./routes/gifts.js";
-import messages from "./routes/messages.js";
-import visitors from "./routes/visitors.js";
-import friends from "./routes/friends.js";
-import forum from "./routes/forum.js";
-import votes from "./routes/votes.js";
-import auth from "./routes/auth.js";
+import photos from ".src/routes/photos.js";
+import gifts from ".src/routes/gifts.js";
+import messages from ".src/routes/messages.js";
+import visitors from ".src/routes/visitors.js";
+import friends from ".src/routes/friends.js";
+import forum from ".src/routes/forum.js";
+import votes from ".src/routes/votes.js";
+import auth from ".src/routes/auth.js";
 
 const app = express();
 app.use(cors());
