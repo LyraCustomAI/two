c948094b2c67a93437c45dd187fc97b2b4e6d29e
LyraCustomAI <essence.littlelady@gmail.com>
2025-12-02 16:55:27 +0200

    Update login.js

--- two-web/js/login.js
+++ two-web/js/login.js
@@ -2,7 +2,7 @@ const license = window.fivem_license || localStorage.getItem("license");
 
 document.querySelector("#login_submit").onclick = async () => {
 
-    const res = await fetch("https://two.lt/api/auth/login", {
+    const res = await fetch("http://194.60.87.77:3000/api/auth/login", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({ license })
